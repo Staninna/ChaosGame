6d9af7fa5f37727dadadc8807da090b6f36a868d
Staninna <26458805+Staninna@users.noreply.github.com>
2022-06-07 12:22:56 +0200

    ⚡️ Improved the performace becaue i understand now how the update function works

--- src/main.rs
+++ src/main.rs
@@ -12,50 +12,59 @@ fn main() {
 struct Model {
     current: Vec2,
     points: Vec<Vec2>,
+    new_points: Vec<Vec2>,
 }
+
+const BUFFER_FRAMES: i32 = 2048;
+const SIZE: i32 = 512;
+const ORIGINS: i32 = 3;
 
 // Ran before drawing
 fn model(app: &App) -> Model {
-    // Configurable parameters
-    let size = 512;
-    let origins = 3;
-
     // Dynamically generated variables
     let mut random = rand::thread_rng();
     let current = pt2(
-        random.gen_range(0.0..size as f32),
-        random.gen_range(0.0..size as f32),
+        random.gen_range(0.0..SIZE as f32),
+        random.gen_range(0.0..SIZE as f32),
     );
-    let radius = size as f32 / 2.0;
+    let radius = SIZE as f32 / 2.0;
     let points = (0..=360)
-        .step_by(360 / origins as usize)
+        .step_by(360 / ORIGINS as usize)
         .map(|i| {
             let radian = deg_to_rad(i as f32);
-            let x = (radian.sin() * radius) + (size / 2) as f32;
-            let y = (radian.cos() * radius) + (size / 2) as f32;
+            let x = (radian.sin() * radius) + (SIZE / 2) as f32;
+            let y = (radian.cos() * radius) + (SIZE / 2) as f32;
             pt2(x, y)
         })
         .collect::<Vec<_>>();
 
     // Make new window
     app.new_window()
-        .size(size, size)
+        .size(SIZE as u32, SIZE as u32)
         .view(view)
         .build()
         .unwrap();
-    Model { current, points }
+    Model {
+        current,
+        points,
+        new_points: Vec::new(),
+    }
 }
 
 // Update app data struct
 fn update(_app: &App, model: &mut Model, _update: Update) {
-    model.current = model.current.lerp(
-        model
-            .points
-            .choose(&mut rand::thread_rng())
-            .unwrap()
-            .clone(),
-        0.5,
-    );
+    model.new_points.clear();
+    for _ in 0..BUFFER_FRAMES {
+        model.current = model.current.lerp(
+            model
+                .points
+                .choose(&mut rand::thread_rng())
+                .unwrap()
+                .clone(),
+            0.5,
+        );
+        model.new_points.push(model.current);
+    }
 }
 
 // Real main function
@@ -66,10 +75,11 @@ fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw().x_y(-win.w() * 0.5, -win.h() * 0.5);
 
     if frame.nth() == 0 {
-        // Draw first frame
-        draw.background().color(PLUM);
+        draw.background().color(BLACK);
     }
 
-    draw.ellipse().radius(0.5).xy(model.current).color(BLUE);
+    for point in model.new_points.iter() {
+        draw.ellipse().radius(0.1).xy(*point).color(BLUE);
+    }
     draw.to_frame(app, &frame).unwrap();
 }
